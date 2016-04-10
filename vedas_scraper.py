daa9bdf91a45ffc283edf1ef2a4bbaec041f2e67
coderbhupendra <birendrachauhan3@gmail.com>
2016-04-10 23:25:09 +0530

    added chikitsa file

--- vedas_scraper.py
+++ vedas_scraper.py
@@ -48,26 +48,27 @@ def write_text(lines,target):
 
 
 def get_links_books():
-	links=["https://sa.wikisource.org/wiki/%E0%A4%85%E0%A4%B7%E0%A5%8D%E0%A4%9F%E0%A4%BE%E0%A4%99%E0%A5%8D%E0%A4%97%E0%A4%B9%E0%A5%83%E0%A4%A6%E0%A4%AF%E0%A4%AE%E0%A5%8D/%E0%A4%B8%E0%A5%82%E0%A4%A4%E0%A5%8D%E0%A4%B0%E0%A4%B8%E0%A5%8D%E0%A4%A5%E0%A4%BE%E0%A4%A8%E0%A4%AE%E0%A5%8D#.E0.A4.85.E0.A4.A7.E0.A5.8D.E0.A4.AF.E0.A4.BE.E0.A4.AF_01"]
-	directory="Vedas/"
+	links=["https://sa.wikisource.org/wiki/%E0%A4%85%E0%A4%B7%E0%A5%8D%E0%A4%9F%E0%A4%BE%E0%A4%99%E0%A5%8D%E0%A4%97%E0%A4%B9%E0%A5%83%E0%A4%A6%E0%A4%AF%E0%A4%AE%E0%A5%8D/%E0%A4%B8%E0%A5%82%E0%A4%A4%E0%A5%8D%E0%A4%B0%E0%A4%B8%E0%A5%8D%E0%A4%A5%E0%A4%BE%E0%A4%A8%E0%A4%AE%E0%A5%8D#.E0.A4.85.E0.A4.A7.E0.A5.8D.E0.A4.AF.E0.A4.BE.E0.A4.AF_01",
+	"https://sa.wikisource.org/wiki/%E0%A4%9A%E0%A4%BF%E0%A4%95%E0%A4%BF%E0%A4%A4%E0%A5%8D%E0%A4%B8%E0%A4%BE%E0%A4%B8%E0%A5%8D%E0%A4%A5%E0%A4%BE%E0%A4%A8%E0%A4%AE%E0%A5%8D"]
+	directory="wiki_doc/"
 	make_dir(directory)
-	url=links[0]
+	url=links[1]
 	
 	html=get_html(url)
 	#html=urllib.urlopen(url)
 	soup = BeautifulSoup(html)
 
-	path_="Vedas/Vedas.txt"
+	path_="wiki_doc/chikitsa.txt"
 	target_= open(path_, 'w')
 
 	text=soup.findAll('p')
 
-	for i in range (3,len(text)):
+	for i in range (1,len(text)):
 		if text[i]!="":
 			line=text[i].get_text()
 			if  line.rstrip():
 				line=re.sub(r"[\u0966-\u096F]+", "", line)
-				line=re.sub(r"(\.|[\u0966-\u096F]*)([A-Za-z])\w*|([\u0966-\u096F]*\.[\u0966-\u096F]*)+|([\s])+॥$", "", line)
+				line=re.sub(r"(\.|[\u0966-\u096F]*)([A-Za-z])\w*|([\u0966-\u096F]*\.[\u0966-\u096F]*)+|([\s])+॥$|[+]", "", line)
 				target_.write(line.strip())
 				target_.write("\n")
 if __name__ == "__main__":
