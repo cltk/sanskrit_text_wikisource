1f788d6b1b835f7fbc899d73918365a036273303
coderbhupendra <birendrachauhan3@gmail.com>
2016-04-10 23:45:45 +0530

    added sutar doc

--- vedas_scraper.py
+++ vedas_scraper.py
@@ -16,16 +16,13 @@ def get_html(url_link):
 	with urllib.request.urlopen(url_link) as url:
 		return url.read()
 def clean_files():
-	directory="Vedas/"
-	path_="Vedas/skandapur_cleaned.txt"
+	directory="wiki_doc/"
+	path_="wiki_doc/sutar.txt"
 	target_= open(path_, 'w')
 
-	with open(directory+"skandapur.txt") as f:
+	with open(directory+"sutar.txt") as f:
 		for line in f:
 			if line.rstrip() !='':
-				line=re.sub(r"[\u0966-\u096F]+", "", line)
-				line=re.sub(r"[.][/]", "", line)
-				line=re.sub(r"(SP)(\w)+(\s)+", "", line)
 				target_.write(line.strip())
 
 				target_.write("\n")
@@ -49,16 +46,17 @@ def write_text(lines,target):
 
 def get_links_books():
 	links=["https://sa.wikisource.org/wiki/%E0%A4%85%E0%A4%B7%E0%A5%8D%E0%A4%9F%E0%A4%BE%E0%A4%99%E0%A5%8D%E0%A4%97%E0%A4%B9%E0%A5%83%E0%A4%A6%E0%A4%AF%E0%A4%AE%E0%A5%8D/%E0%A4%B8%E0%A5%82%E0%A4%A4%E0%A5%8D%E0%A4%B0%E0%A4%B8%E0%A5%8D%E0%A4%A5%E0%A4%BE%E0%A4%A8%E0%A4%AE%E0%A5%8D#.E0.A4.85.E0.A4.A7.E0.A5.8D.E0.A4.AF.E0.A4.BE.E0.A4.AF_01",
-	"https://sa.wikisource.org/wiki/%E0%A4%9A%E0%A4%BF%E0%A4%95%E0%A4%BF%E0%A4%A4%E0%A5%8D%E0%A4%B8%E0%A4%BE%E0%A4%B8%E0%A5%8D%E0%A4%A5%E0%A4%BE%E0%A4%A8%E0%A4%AE%E0%A5%8D"]
+	"https://sa.wikisource.org/wiki/%E0%A4%9A%E0%A4%BF%E0%A4%95%E0%A4%BF%E0%A4%A4%E0%A5%8D%E0%A4%B8%E0%A4%BE%E0%A4%B8%E0%A5%8D%E0%A4%A5%E0%A4%BE%E0%A4%A8%E0%A4%AE%E0%A5%8D",
+	"https://sa.wikisource.org/wiki/%E0%A4%9A%E0%A4%B0%E0%A4%95%E0%A4%B8%E0%A4%82%E0%A4%B9%E0%A4%BF%E0%A4%A4%E0%A4%BE/%E0%A4%B8%E0%A5%82%E0%A4%A4%E0%A5%8D%E0%A4%B0%E0%A4%B8%E0%A5%8D%E0%A4%A5%E0%A4%BE%E0%A4%A8%E0%A4%AE%E0%A5%8D"]
 	directory="wiki_doc/"
 	make_dir(directory)
-	url=links[1]
+	url=links[2]
 	
 	html=get_html(url)
 	#html=urllib.urlopen(url)
 	soup = BeautifulSoup(html)
 
-	path_="wiki_doc/chikitsa.txt"
+	path_="wiki_doc/sutar.txt"
 	target_= open(path_, 'w')
 
 	text=soup.findAll('p')
@@ -67,9 +65,13 @@ def get_links_books():
 		if text[i]!="":
 			line=text[i].get_text()
 			if  line.rstrip():
+
 				line=re.sub(r"[\u0966-\u096F]+", "", line)
-				line=re.sub(r"(\.|[\u0966-\u096F]*)([A-Za-z])\w*|([\u0966-\u096F]*\.[\u0966-\u096F]*)+|([\s])+॥$|[+]", "", line)
+				line=re.sub(r"(\.|[\u0966-\u096F]*)([A-Za-z])\w*|([\u0966-\u096F]*\.[\u0966-\u096F]*)+|([\s])+॥$|[+]", "", line)#0,1,2
+				line=re.sub(r"(।।)[\s]*([\u0966-\u096F]*)[\s]*(।।)|(।।)", "॥", line) #2
+				line=re.sub(r"[)]|चसं-, ॥", "", line)                                         #2  
 				target_.write(line.strip())
 				target_.write("\n")
 if __name__ == "__main__":
 	get_links_books()
+	#clean_files()
